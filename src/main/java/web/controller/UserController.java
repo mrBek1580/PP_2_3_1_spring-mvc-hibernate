aae2a975941048a027af72c7110b9279db91412a
GelbeDuck <superserge.k@gmail.com>
2024-02-25 22:09:15 +0300

    commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,53 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "index";
+    }
+
+    @GetMapping("/adduser")
+    public String CreateUserForm(@ModelAttribute("user") User user) {
+        return "adduser";
+    }
+
+    @PostMapping("/adduser")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/deleteUser")
+    public String deleteUser(@RequestParam("id") long id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/updateuser")
+    public String getEditUserForm(Model model, @RequestParam("id") long id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "updateuser";
+    }
+
+    @PostMapping("/updateuser")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/";
+    }
+}
